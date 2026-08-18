089e2e6b69424fecd7101cffa0cde074eae9e9cd
Danilorachadel-j <danilo_rachadel-j@>
2026-08-18 08:03:53 -0300

    add - finalizei o banco de dados

--- database/db.sql
+++ database/db.sql
@@ -4,5 +4,7 @@ USE cadastro_pratos;
 CREATE TABLE cardapio (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(250) NOT NULL,
-    
-)
+    preco DECIMAL(5, 2) NOT NULL,
+    descricao VARCHAR (250) NOT NULL,
+    categoria VARCHAR(250 ) NOT NULL
+);
